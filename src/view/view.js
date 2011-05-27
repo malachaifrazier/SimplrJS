90ec0eb0eda8853592194717ba38a219c180f724
Brian Ritter <ritterb82@gmail.com>
2011-05-27 19:09:14 -0400

    update for Simplr.View for capitalization

--- src/view/view.js
+++ src/view/view.js
@@ -1,27 +1,25 @@
-(function($) {
+(function() {
 	
-	var data = {};
+	var ViewData = {};
 	
-	$.extend(true, simplr, {
-		view : {
-			mAddViews : function(obj) {
-				for(var key in obj) {
-					var newView = $.extend(true, {}, { 
-						html : function(data) { return ""; }, 
-						callback : function(selector, data) {} 
-					}, obj[key]);
-					data[key] = newView;
-				}
-			},
-			mGetViews : function() {
-				return data;
-			},
-			mRender : function(options) {
-				var tmp = $.extend({ name : "", data: "", selector : ""}, options);
-				$(tmp.selector).html(data[tmp.name].html(tmp.data));
-				data[tmp.name].callback(tmp.selector, tmp.data);
+	Simplr.View = {
+		mAddViews : function(obj) {
+			for(var key in obj) {
+				var newView = $.extend(true, {}, { 
+					html : function(data) { return ""; }, 
+					callback : function(selector, data) {} 
+				}, obj[key]);
+				ViewData[key] = newView;
 			}
+		},
+		mGetViews : function() {
+			return ViewData;
+		},
+		mRender : function(options) {
+			var tmp = $.extend({ name : "", data: "", selector : ""}, options);
+			$(tmp.selector).html(ViewData[tmp.name].html(tmp.data));
+			ViewData[tmp.name].callback(tmp.selector, tmp.data);
 		}
-	});
+	};
 	
-})(jQuery);
+})();
